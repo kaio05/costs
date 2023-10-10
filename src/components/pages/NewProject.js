f1f13456a864855e967bbf23665ebc27080db3ca
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-10-09 21:01:41 -0300

    edit page

---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom'
+
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
+function NewProject() {
+
+    const navigate = useNavigate()
+
+    function createPost(project) {
+
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project)
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                // redirect
+                navigate('/projects', {state:{message: 'Projeto criado com sucesso!'}})
+            })
+            .catch(err => console.log(err))
+
+    }
+
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText='Criar projeto' />
+        </div>
+    )
+}
+
+export default NewProject
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+function ProjectForm({ handleSubmit, btnText, projectData }) {
+    const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+        console.log(project)
+        handleSubmit(project)
+    }
+
+    function handleChange(e) {
+        setProject({...project, [e.target.name]: e.target.value})
+    }
+
+    function handleCategory(e) {
+        setProject({...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text
+        }})
+    }
+
+    return (
+        <form onSubmit={submit} className={styles.form}>
+            <div>
+                <Input type='text' placeholder='Insira o nome do projeto' name='name' text='Nome do projeto' handleOnchange={handleChange} value={project.name ? project.name : ''} />
+            </div>
+            <div>
+                <Input type='number' placeholder='Insira o orçamento total' name='budget' text='Orçamento do projeto' handleOnchange={handleChange} value={project.budget ? project.budget : ''} />
+            </div>
+                <Select name='category_id' text='Selecione a categoria' options={categories} handleOnChange={handleCategory} value={project.category ? project.category.id : ''} />
+            <div>
+                <SubmitButton text={btnText} />
+            </div>
+        </form>
+    )
+}
+
+export default ProjectForm
